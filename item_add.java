549035af700e669443ade3cd59656179db747a90
Dheera13 <46899996+Dheera13@users.noreply.github.com>
2020-08-15 15:33:37 +0530

    Create item_add.java

---
+++ item_add.java
@@ -0,0 +1,13 @@
+package com.example.airqualitymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_add);
+    }
+}
