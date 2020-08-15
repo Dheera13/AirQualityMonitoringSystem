fa4d28c5b19b423fac5bb38b74d9d65ef6061654
Dheera13 <46899996+Dheera13@users.noreply.github.com>
2020-08-15 15:15:57 +0530

    Create activity_screen_slide.java

---
+++ activity_screen_slide.java
@@ -0,0 +1,13 @@
+package com.example.airqualitymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_screen_slide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+    }
+}
