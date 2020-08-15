c38152ac8285823f8743cab631224d0d13642ab8
Dheera13 <46899996+Dheera13@users.noreply.github.com>
2020-08-15 15:17:42 +0530

    Create advisory.java

---
+++ advisory.java
@@ -0,0 +1,14 @@
+package com.example.airqualitymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class advisory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.advisory);
+    }
+}
