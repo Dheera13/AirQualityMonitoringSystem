16dd1794ab9f57c2db5982fe12b6a146ed51d9ac
Dheera13 <46899996+Dheera13@users.noreply.github.com>
2020-08-15 15:19:22 +0530

    Create album_card.java

---
+++ album_card.java
@@ -0,0 +1,13 @@
+package com.example.airqualitymonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class album_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album_card);
+    }
+}
